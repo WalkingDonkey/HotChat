39d17945ee265642e7c23dc6750efc2bf0cefff2
ychenu <jsnt_cy@hotmail.com>
2015-12-29 19:06:53 +0800

    Update repository

--- HotChat/HotChat.Repository.Mongo/Abstract/Repository.cs
+++ HotChat/HotChat.Repository.Mongo/Abstract/Repository.cs
@@ -3,39 +3,56 @@ using MongoDB.Driver;
 
 namespace HotChat.Repository.Mongo.Abstract
 {
-    public abstract class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class
-    {
-        private static IMongoDatabase _db;
-        private string _collectionName;
+   public abstract class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class
+   {
+      private static IMongoDatabase _db;
+      private string _collectionName;
 
-        public Repository(string collectionName)
-        {
-            _collectionName = collectionName;
-        }
+      public Repository(string collectionName)
+      {
+         _collectionName = collectionName;
+      }
 
-        static Repository()
-        {
-            var client = new MongoClient("mongodb://127.0.0.1:27017");
-            _db = client.GetDatabase("HotChat");
-        }
+      static Repository()
+      {
+         var client = new MongoClient("mongodb://127.0.0.1:27017");
+         _db = client.GetDatabase("HotChat");
+      }
 
-        public IMongoCollection<TEntity> GetCollection()
-        {
-            return _db.GetCollection<TEntity>(_collectionName);
-        }
+      public IMongoCollection<TEntity> GetCollection()
+      {
+         return _db.GetCollection<TEntity>(_collectionName);
+      }
 
-        public virtual void Add(TEntity entity)
-        {
-            IMongoCollection<TEntity> collection = GetCollection();
-            collection.InsertOne(entity);
-        }
+      public virtual void Add(TEntity entity)
+      {
+         IMongoCollection<TEntity> collection = GetCollection();
+         collection.InsertOne(entity);
+      }
 
-        public virtual void Delete(TKey key)
-        {
-        }
+      public virtual void Delete(TKey key)
+      {
+      }
 
-        public virtual void Update(TKey key)
-        {
-        }
-    }
+      //public virtual void Update(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> updater)
+      //{
+      //   GetCollection().FindOneAndUpdate(filter, updater);
+      //}
+
+      public virtual bool Exist<T>(T filter)
+      {
+         return true;
+         //return GetCollection().Count(filter) != 0;
+      }
+
+      protected FilterDefinition<TEntity> EqFilter<TValue>(string field, TValue value)
+      { 
+         return Builders<TEntity>.Filter.Eq(field, value);
+      }
+
+      protected UpdateDefinition<TEntity> PushUpdater<TValue>(string field, TValue value)
+      {
+         return Builders<TEntity>.Update.Push(field, value);
+      }
+   }
 }
--- HotChat/HotChat.Repository/IRepository.cs
+++ HotChat/HotChat.Repository/IRepository.cs
@@ -1,14 +1,15 @@
 ﻿namespace HotChat.Repository.Interface
 {
-    public interface IRepository<TEntity, TKey> where TEntity : class
-    {
-        //IList<TEntity> GetAll();
-        //IList<TEntity> GetAll(int pageIndex, int pageSize);
-        //TEntity GetByID(TKey id);
+   public interface IRepository<TEntity, TKey> where TEntity : class
+   {
+      //IList<TEntity> GetAll();
+      //IList<TEntity> GetAll(int pageIndex, int pageSize);
+      //TEntity GetByID(TKey id);
 
-        // CRUD
-        void Add(TEntity entity);
-        void Delete(TKey key);
-        void Update(TKey key);
-    }
+      // CRUD
+      void Add(TEntity entity);
+      void Delete(TKey key);
+      //void Update<T1, T2>(T1 query1,T2 query2);
+      bool Exist<T>(T query);
+   }
 }
